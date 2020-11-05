1b79513ed79526b87bbc562d6a576d25721d691b
dave singh <dharmender@hotmail.com>
2020-11-05 05:32:08 -0600

    initializing repository

--- app.py
+++ app.py
@@ -1,23 +1,63 @@
 from flask import Flask
 import os
 from decouple import config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = Flask(__name__)
+
 appSetting = config('CONFIGURATION_SETUP')
-
-print(appSetting)   #prints: config.DevelopmentConfig
-
-
 app.config.from_object(appSetting)  
 
-app.config['SECRET_KEY'] = config('SECRET_KEY')
-
-print (app.config)
-
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 #print(f"Development: {app.config['DEVELOPMENT']}")
 #print(f"Debug: {app.config['DEBUG']}")
-print(f"Secret key: {app.config['SECRET_KEY']}")
+#print(f"Secret key: {app.config['SECRET_KEY']}")
 
+class CarsModel(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    model = db.Column(db.String())
+    doors = db.Column(db.Integer())
+
+    def __init__(self, name, model, doors):
+        self.name = name
+        self.model = model
+        self.doors = doors
+
+    def __repr__(self):
+        return f"<Car {self.name}>"
+
+@app.route('/cars', methods=['POST', 'GET'])
+def handle_cars():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_car = CarsModel(name=data['name'], model=data['model'], doors=data['doors'])
+            db.session.add(new_car)
+            db.session.commit()
+            return {"message": f"car {new_car.name} has been created successfully."}
+        else:
+            return {"error": "The request payload is not in JSON format"}
+
+    elif request.method == 'GET':
+        cars = CarsModel.query.all()
+        results = [
+            {
+                "name": car.name,
+                "model": car.model,
+                "doors": car.doors
+            } for car in cars]
+
+        return {"count": len(results), "cars": results}
+
+
+"""
 @app.route('/')
 def hello():
     return "Hello World!"
@@ -25,6 +65,7 @@ def hello():
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+"""
 
 if __name__ == '__main__':
     app.run()
--- config.py
+++ config.py
@@ -1,31 +1,21 @@
 import os
-from os import environ
+from decouple import config
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+#basedir = os.path.abspath(os.path.dirname(__file__))
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-"""
-    Set Flask configuration vars from .env file
-
-    # General
-    TESTING = environ["TESTING"]
-    FLASK_DEBUG = environ["FLASK_DEBUG"]     //index into dictionary
-    SECRET_KEY = environ.get('SECRET_KEY')
-
-# Database
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
-"""
 class Config(object):
     DEBUG = False
     TESTING = False
     DEVELOPMENT = False
     CSRF_ENABLED = True
-    
- 
-#    SECRET_KEY = os.environ['AWS_SECRET_KEY']
-#    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = config('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS')
+    print(SQLALCHEMY_DATABASE_URI)
 
 class ProductionConfig(Config):
     DEBUG = False
